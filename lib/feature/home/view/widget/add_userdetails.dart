92461cff936ffa7532b554225742d434324374df
sayanths <sayanthsayu619@email.com>
2023-11-04 15:09:02 +0530

    adding view done

--- lib/feature/home/view/widget/add_userdetails.dart
+++ lib/feature/home/view/widget/add_userdetails.dart
@@ -3,15 +3,17 @@ import 'package:firebase_todo/core/color/color.dart';
 import 'package:firebase_todo/core/custom_size/custom_size.dart';
 import 'package:firebase_todo/core/custom_textfield/custom_textfield.dart';
 import 'package:firebase_todo/core/google_fonts/google_fonts.dart';
+import 'package:firebase_todo/feature/home/view_model/home_controller.dart';
 import 'package:firebase_todo/responsive/responsive.dart';
-import 'package:firebase_todo/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AddUserDetails extends StatelessWidget {
   const AddUserDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final data = context.read<HomeController>();
     return Scaffold(
       backgroundColor: Apc.white,
       appBar: AppBar(
@@ -22,26 +24,37 @@ class AddUserDetails extends StatelessWidget {
           style: K2DFonts.bold(color: Apc.white),
         ),
       ),
-      body: Column(
-        children: [
-          heightMedium,
-          const CustomTextField(
-            title: 'Name',
-            textInputType: TextInputType.name,
-          ),
-          const CustomTextField(
-            title: 'Email',
-            textInputType: TextInputType.emailAddress,
-          ),
-          const CustomTextField(
-            title: 'Age',
-            textInputType: TextInputType.number,
-          ),
-          const CustomTextField(
-            title: 'Number',
-            textInputType: TextInputType.phone,
-          )
-        ],
+      body: Form(
+        key: data.todoAdding,
+        child: Column(
+          children: [
+            heightMedium,
+            CustomTextField(
+              controller: data.nameField,
+              validate: (val) => data.validateName(val),
+              title: 'Name',
+              textInputType: TextInputType.name,
+            ),
+            CustomTextField(
+              validate: (val) => data.validateEmail(val),
+              controller: data.emailField,
+              title: 'Email',
+              textInputType: TextInputType.emailAddress,
+            ),
+            CustomTextField(
+              validate: (val) => data.validateAge(val),
+              controller: data.ageField,
+              title: 'Age',
+              textInputType: TextInputType.number,
+            ),
+            CustomTextField(
+              validate: (val) => data.validatePhoneNumber(val),
+              controller: data.numnerField,
+              title: 'Number',
+              textInputType: TextInputType.phone,
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -51,7 +64,9 @@ class AddUserDetails extends StatelessWidget {
           width: Responsive.widthMultiplier! * 83,
           height: Responsive.heightMultiplier! * 5,
           onPressed: () {
-            Routes.back();
+            if (data.todoAdding.currentState!.validate()) {
+              print("object");
+            }
           },
         ),
       ),
--- lib/feature/home/view_model/home_controller.dart
+++ lib/feature/home/view_model/home_controller.dart
@@ -22,4 +22,89 @@ class HomeController extends ChangeNotifier {
     'assets/amount recive.png',
     'assets/recive.png',
   ];
+
+  final todoAdding = GlobalKey<FormState>();
+  final TextEditingController _nameField = TextEditingController();
+  TextEditingController get nameField => _nameField;
+  final TextEditingController _emailField = TextEditingController();
+  TextEditingController get emailField => _emailField;
+  final TextEditingController _ageField = TextEditingController();
+  TextEditingController get ageField => _ageField;
+  final TextEditingController _numnerField = TextEditingController();
+  TextEditingController get numnerField => _numnerField;
+
+  String? validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your name';
+    }
+
+    final specialCharacters = RegExp(r'[!@#%^&*(),.?":{}|<>]');
+
+    if (value.contains(specialCharacters)) {
+      return 'Special characters are not allowed';
+    }
+    if (RegExp(r'[0-9]').hasMatch(value)) {
+      return 'Numeric values are not allowed';
+    }
+
+    return null;
+  }
+
+  String? validatePhoneNumber(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your number';
+    }
+
+    final numericValue = value.replaceAll(RegExp(r'[^0-9]'), '');
+
+    if (numericValue.length != 10) {
+      return 'Please enter a 10-digit number';
+    }
+
+    // Check if the numericValue is a repetitive sequence
+    if (RegExp(
+            r'^(0{10}|1{10}||2{10}|3{10}|3{10}|4{10}|5{10}|6{10}|7{10}|8{10}|9{10}|1234567890|0123456789)$')
+        .hasMatch(numericValue)) {
+      return 'Invalid number';
+    }
+
+    return null;
+  }
+
+  String? validateAge(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your number';
+    }
+
+    final numericValue = value.replaceAll(RegExp(r'[^0-9]'), '');
+
+    // ignore: prefer_is_empty
+    if (numericValue.length <= 0) {
+      return 'Please enter proper age';
+    }
+    // Check if the numericValue is a repetitive sequence
+    if (RegExp(
+            r'^(0{10}|1{10}||2{10}|3{10}|3{10}|4{10}|5{10}|6{10}|7{10}|8{10}|9{10}|1234567890|0123456789)$')
+        .hasMatch(numericValue)) {
+      return 'Invalid number';
+    }
+
+    return null;
+  }
+
+  String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter an email address';
+    }
+    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+    final regex = RegExp(pattern);
+
+    return !regex.hasMatch(value) ? 'Enter a valid email address' : null;
+  }
 }
